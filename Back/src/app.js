8401d31784b3b1042503e9faeb665b74d002e831
claudio <contacto.claudio@gmail.com>
2023-08-30 15:57:37 -0400

    se quira cors y se agrega manual 2

--- Back/src/app.js
+++ Back/src/app.js
@@ -13,7 +13,7 @@ import sliderHomeRoutes from "./routes/sliderhome.routes.js"
 const app = express()
 
 
-server.app((req, res, next) => {
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
