467aabf6ce40c8369aef99a4d3854e7a543ec8c0
claudio <contacto.claudio@gmail.com>
2023-08-30 15:55:23 -0400

    se quira cors y se agrega manual

--- Back/src/app.js
+++ Back/src/app.js
@@ -12,12 +12,21 @@ import sliderHomeRoutes from "./routes/sliderhome.routes.js"
 
 const app = express()
 
-app.use(cors({
+
+server.app((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  });
+
+/* app.use(cors({ */
     /* origin: 'http://localhost:5173', */
     /* origin: 'https://magyprint.cl', */
-    origin: '*',
+  /*   origin: '*', */
     /* credentials: true */
-}))
+/* })) */
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(fileUpload({
