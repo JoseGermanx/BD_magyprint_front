d2eb72ee34a85956a898b15f44b4673c7ae62d41
Jose Germán Martínez <jgermanmm@gmail.com>
2023-08-30 16:09:48 -0400

    Update axios.js

--- Front/src/api/axios.js
+++ Front/src/api/axios.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     /* baseURL: 'http://localhost:3090/api', */
-    baseURL: 'https://magyprintbd.onrender.com/api',
-    withCredentials: true
+    baseURL: 'https://magyprintback.onrender.com/api'
 })
 
 
